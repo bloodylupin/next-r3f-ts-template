4f21f858512160eaaedd6f0029f522d1cde5f244
bloodylupin <andrea.borin.ndr@gmail.com>
2023-11-04 01:28:33 +0100

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-import dynamic from "next/dynamic";
-const Scene = dynamic(() => import("./components/canvas/Scene"), {
-  ssr: false,
-  loading: () => (
-    <div className="h-full grid">
-      <div className="m-auto">
-        <span className="loading loading-spinner" />
-      </div>
-    </div>
-  ),
-});
-// import Scene from "./components/canvas/Scene";
+import Scene from "./components/canvas/Scene";
 
 export default function Home() {
   return (
